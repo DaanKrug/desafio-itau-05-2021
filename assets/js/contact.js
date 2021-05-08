857728391c0a258ce80e6d399c681f341b7e2ce5
DaanKrug <daniel-krug@hotmail.com>
2021-05-08 18:57:26 -0300

    feat: contact form submit action added

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -1,5 +1,8 @@
+// https://webhook.site/#!/99cf7ed2-b14f-4745-84ba-9478534b0594/d0efac75-dff3-4a75-891c-e9dcbd156802/1
+var webhookUrl = 'https://webhook.site/99cf7ed2-b14f-4745-84ba-9478534b0594';
+var webhookEmail = '99cf7ed2-b14f-4745-84ba-9478534b0594@email.webhook.site';
+
 document.addEventListener('submit',(event) => {
-	console.log(event);
 	event.preventDefault();
 	if(event.target.id != 'contactForm'){
 		return;
@@ -7,5 +10,18 @@ document.addEventListener('submit',(event) => {
 	var name = document.getElementById('name').value;
 	var email = document.getElementById('email').value;
 	var message = document.getElementById('message').value;
-	console.log([name,email,message]);
+	fetch(webhookUrl,{
+		method: 'POST',
+		body: {
+			name: name,
+			email: email,
+			message: message
+		}
+	}).then(response => {
+		if(response.status === 200){
+			alert('Sucesso ao Enviar Contato');
+			return;
+		}
+		alert('Erro ao Enviar Contato');
+	});
 });
